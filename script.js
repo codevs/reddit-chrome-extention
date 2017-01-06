2df7e54c8eac734b0eb892e750961914a39de12a
Aleksandr Vasilenko <vasilenko.alex93@gmail.com>
2017-01-05 17:52:17 -0800

    Started using Reddit API

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 $(document).ready(function() {
-	$('#text').text("Hello World");
-	chrome.browserAction.setBadgeBackgroundColor({color: [255,0,0,255]});
-  	chrome.browserAction.setBadgeText({text: "10"});
-  	 $.getJSON(
-        "http://www.reddit.com/r/pics.json?jsonp=?",
-        function foo(data)
-        {
-          $.each(
-            data.data.children.slice(0, 10),
-            function (i, post) {
-              $("#text").append( '<br>' + post.data.title );
-              $("#text").append( '<br>' + post.data.url );
-              $("#text").append( '<br>' + post.data.permalink );
-              $("#text").append( '<br>' + post.data.ups );
-              $("#text").append( '<br>' + post.data.downs );
-              $("#text").append( '<hr>' );
+    $.ajax({
+        url: "http://www.reddit.com/r/GlobalOffensive/new.json",
+        type: 'GET',
+        dataType: 'json',
+        beforeSend : function(xhr) {
+            // xhr.setRequestHeader("Authorization", "Bearer " + token);
+        },
+        success: function(data, textStatus, jqXHR) {
+            if(jqXHR.status == 200) {
+                $("#title").text(data.data.children[0].data.title);
             }
-          )
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            // Reload the page if the server went down
+            if(jqXHR.readyState == 0 && jqXHR.status == 0) {
+                // REMOVE THIS
+            }
+
+            // Check for Unauthorized
+            if(jqXHR.status == 401) {
+                // REMOVE THIS
+            }
+
+            // Handle server 500
+            if(jqXHR.status == 500) {
+                // TODO
+            }
         }
-      )
-      .success(function() { alert("second success"); })
-      .error(function() { alert("error"); })
-      .complete(function() { alert("complete"); });
+    });
+
 });
