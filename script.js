dc540c3197365fc144b764ed0a7c23e8b2b336a6
Aleksandr Vasilenko <vasilenko.alex93@gmail.com>
2017-01-06 13:02:12 -0800

    Consistent spacing + editorconfig file
    Lets use spaces instead of tabs, it is more consistent this way

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// We should stick to TAB being two spaces. Not actual tabs
+// if your editor allows for .editorconfig please turn it on
+// look up what it is. It changes the settings of an editor so they are the
+// same between computers
+
 $(document).ready(function() {
 	chrome.browserAction.setBadgeBackgroundColor({color: [255,0,0,255]});
 	//chrome.browserAction.setBadgeText({text: "10"});
@@ -7,49 +12,48 @@ $(document).ready(function() {
 
 	var URL = base_url + subreddit + "/" + sorting + ".json";
 
-    $.ajax({
-				url: URL,
-        type: 'GET',
-        dataType: 'json',
-        beforeSend : function(xhr) {
-            // xhr.setRequestHeader("Authorization", "Bearer " + token);
-        },
-        success: function(data, textStatus, jqXHR) {
-			var count = 0;
-			var posts = data.data.children;
-            if(jqXHR.status == 200) {
-				var number = 1;
-				for(var i = 0; i < posts.length; i++){
-					if(posts[i].data.clicked === false){
-						count++;
-						//dict["" + posts[i].data.title] = "" + posts[i].data.selftext;
-						var $item = $("#post").clone();
-						$item.find("#title").text("" + number++ + ") " + posts[i].data.title);
-						if(posts[i].data.selftext_html !== null){
-							$item.find("#text").text("" + posts[i].data.selftext.substring(0, 90) + "...");
-						}
-						$item.appendTo("#list");
-					}
-				}
+  $.ajax({
+    url: URL,
+    type: 'GET',
+    dataType: 'json',
+    beforeSend : function(xhr) {
+       // xhr.setRequestHeader("Authorization", "Bearer " + token);
+    },
+    success: function(data, textStatus, jqXHR) {
+	    var count = 0;
+      var posts = data.data.children;
+      if(jqXHR.status == 200) {
+        var number = 1;
+        for(var i = 0; i < posts.length; i++) {
+          if(posts[i].data.clicked === false) {
+            count++;
+            //dict["" + posts[i].data.title] = "" + posts[i].data.selftext;
+            var $item = $("#post").clone();
+            $item.find("#title").text("" + number++ + ") " + posts[i].data.title);
+            if(posts[i].data.selftext_html !== null) {
+              $item.find("#text").text("" + posts[i].data.selftext.substring(0, 90) + "...");
             }
-			chrome.browserAction.setBadgeText({text: "" + count});
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            // Reload the page if the server went down
-            if(jqXHR.readyState == 0 && jqXHR.status == 0) {
-                // REMOVE THIS
-            }
-
-            // Check for Unauthorized
-            if(jqXHR.status == 401) {
-                // REMOVE THIS
-            }
-
-            // Handle server 500
-            if(jqXHR.status == 500) {
-                // TODO
-            }
+            $item.appendTo("#list");
+          }
         }
-    });
+      }
+      chrome.browserAction.setBadgeText({text: "" + count});
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      // Reload the page if the server went down
+      if(jqXHR.readyState == 0 && jqXHR.status == 0) {
+        // REMOVE THIS
+      }
 
+      // Check for Unauthorized
+      if(jqXHR.status == 401) {
+        // REMOVE THIS
+      }
+
+      // Handle server 500
+      if(jqXHR.status == 500) {
+        // TODO
+      }
+    }
+  });
 });
