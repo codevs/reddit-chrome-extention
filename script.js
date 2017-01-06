dd22367e3372128439e5baa88fd3321928732260
Daniel Akhterov <akhterovd@gmail.com>
2017-01-05 20:51:55 -0800

    Changed Subreddit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	var url = "http://www.reddit.com/r/";
 	
     $.ajax({
-        url: "http://www.reddit.com/r/GlobalOffensive/new.json",
+        url: "http://www.reddit.com/r/ClassicOffensive/new.json",
         type: 'GET',
         dataType: 'json',
         beforeSend : function(xhr) {
