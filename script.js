ad1833c29fd1ef75c6ebc39c2988c59ba2de4b67
Aleksandr Vasilenko <vasilenko.alex93@gmail.com>
2017-01-05 20:23:30 -0800

    added the reddit api to script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+	chrome.browserAction.setBadgeBackgroundColor({color: [255,0,0,255]});
+	//chrome.browserAction.setBadgeText({text: "10"});
+	var url = "http://www.reddit.com/r/";
+	
     $.ajax({
         url: "http://www.reddit.com/r/GlobalOffensive/new.json",
         type: 'GET',
@@ -7,9 +11,24 @@ $(document).ready(function() {
             // xhr.setRequestHeader("Authorization", "Bearer " + token);
         },
         success: function(data, textStatus, jqXHR) {
+			var count = 0;
+			var posts = data.data.children;
             if(jqXHR.status == 200) {
-                $("#title").text(data.data.children[0].data.title);
+				var number = 1;
+				for(var i = 0; i < posts.length; i++){
+					if(posts[i].data.clicked === false){
+						count++;
+						//dict["" + posts[i].data.title] = "" + posts[i].data.selftext;
+						var $item = $("#post").clone();
+						$item.find("#title").text("" + number++ + ") " + posts[i].data.title);
+						if(posts[i].data.selftext_html !== null){
+							$item.find("#text").text("" + posts[i].data.selftext.substring(0, 90) + "...");
+						}
+						$item.appendTo("#list");
+					}
+				}
             }
+			chrome.browserAction.setBadgeText({text: "" + count});
         },
         error: function(jqXHR, textStatus, errorThrown) {
             // Reload the page if the server went down
