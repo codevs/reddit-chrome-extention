0fff0d242683885dc9fcb2393e03e80aa5cbdf9f
andrix10 <ender2013@gmail.com>
2017-01-08 17:37:31 -0800

    same as master

--- script.js
+++ script.js
@@ -4,41 +4,6 @@
 // same between computers
 
 $(document).ready(function() {
-  chrome.browserAction.setBadgeBackgroundColor({color: [255,0,0,255]});
-  //chrome.browserAction.setBadgeText({text: "10"});
-  const base_url = "http://www.reddit.com/r/";
-  var subreddit = "ClassicOffensive";
-  var sorting = "new";
-
-  var URL = base_url + subreddit + "/" + sorting + ".json";
-
-  $.ajax({
-    url: URL,
-    type: 'GET',
-    dataType: 'json',
-    beforeSend : function(xhr) {
-       // xhr.setRequestHeader("Authorization", "Bearer " + token);
-    },
-    success: function(data, textStatus, jqXHR) {
-      var count = 0;
-      var posts = data.data.children;
-      if(jqXHR.status == 200) {
-        var number = 1;
-        for(var i = 0; i < posts.length; i++) {
-          if(posts[i].data.clicked === false) {
-            count++;
-            //dict["" + posts[i].data.title] = "" + posts[i].data.selftext;
-            var $item = $("#post").clone();
-            $item.find("#title").text("" + number++ + ") " + posts[i].data.title);
-            $item.click(function() {
-              //window.open("reddit.com");
-              chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {url: "reddit.com"}, null);
-              });
-            });
-            //var $win = $item.find("a").attr( "href","reddit.com" + posts[i].data.permalink);
-            if(posts[i].data.selftext_html !== null) {
-              $item.find("#text").text("" + posts[i].data.selftext.substring(0, 90) + "...");
     chrome.browserAction.setBadgeBackgroundColor({color: [255,0,0,255]});
     //chrome.browserAction.setBadgeText({text: "10"});
     const base_url = "http://www.reddit.com/r/";
@@ -123,7 +88,6 @@ $(document).ready(function() {
         //setTimeout(checkForNewPosts, 1000);
         
     }
-  });
     function sortingChanged(){
         sorting = $("#sorting").val();
         console.log("sorting: " + sorting);
